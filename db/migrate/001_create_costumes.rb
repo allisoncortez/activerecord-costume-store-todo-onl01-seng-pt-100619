ffa584d13f9bf907d2671970181738d16e52c9ee
allison cortez <allizetorc@gmail.com>
2020-01-09 02:41:46 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -12,9 +12,9 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
     create_table :costumes do |t|
       t.string :name
-      t.string :price
+      t.string :size
       t.string :image_url
-      t.float :size
+      t.float :prize
       t.timestamps
     end
   end
