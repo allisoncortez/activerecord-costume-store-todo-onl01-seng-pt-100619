2092eaeb1a4899881a9797723202bea5d01a5619
allison cortez <allizetorc@gmail.com>
2020-01-09 02:45:35 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,14 @@
 # Create your costume_stores migration here
+
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.string :size
+      t.string :image_url
+      t.float :price
+      
+      t.timestamps
+    end
+  end
+end
